de9844a003f98dc814c5d58620bff2ef58a0cbbb
Lee Gauthier <lgauthie@uvic.ca>
2012-06-27 21:04:04 -0700

    fixed some whitespace and comments

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ def main():
     # This is where we set up the synth we created
     synth = FM()
     synth.update_envs( at1 = 0.03, at2 = 0.03, de1 = 0.15, de2 = 0.3, re1 = 0.1, re2 = 0.1 )
-    synth.update_ratios( 1, 1.0/6 )
+    synth.set_ratios( 1, 1.0/6 )
     synth.set_gain( ga1 = 1.0, ga2 = 0.2 )
     synth.note_on()
 
-    # Link the mod depth to our envelope
+    # This is where we initialize the envelopes to modulate
+    # the mod index 
     modenv1 = ADSR(synth, "mrs_real/Osc1mDepth", dtime = 0.15, scale = 250 * 2.66)
     modenv2 = ADSR(synth, "mrs_real/Osc2mDepth", dtime = 0.3,  scale = 250 * 1.8)
 
@@ -35,7 +36,7 @@ def main():
                 nton = 'off'
             time = time + synth.tstep
 
-def trig( fr1, fr2, synth, modenv1, modenv2 ):
+def trig(fr1, fr2, synth, modenv1, modenv2):
     synth.update_oscs( fr1, fr2 )
     modenv1.__init__(synth, "mrs_real/Osc1mDepth", dtime = 0.15, scale = fr1 * 2.66)
     modenv2.__init__(synth, "mrs_real/Osc2mDepth", dtime = 0.3,  scale = fr2 * 1.8)
